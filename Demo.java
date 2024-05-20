86db13720afeb1b435936b6e7a8746681036ad54
Bhargava Sai Nadendla <nadendlabhargavsai@gmail.com>
2024-05-20 15:49:35 -0400

    Variables

---
+++ Demo.java
@@ -0,0 +1,14 @@
+class world{
+    public static void main(String[] args){
+        
+        byte b= 120;
+        short s = 211;
+        int num=5; //Binary- 0b101  Hexadecimal- 0x7E
+        long l =54367l;
+        float f = 52.8f;
+        double d = 12e11; 
+        boolean o =true;
+
+        System.out.println(":== " +d+" Hello World!!!");
+    }
+}
