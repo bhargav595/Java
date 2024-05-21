fe8a50edb6e4a1155ba53057d5b1be583db4ca06
Bhargava Sai Nadendla <nadendlabhargavsai@gmail.com>
2024-05-20 22:42:13 -0400

    Type Conversion

--- Demo.java
+++ Demo.java
@@ -3,12 +3,19 @@ class world{
         
         byte b= 120;
         short s = 211;
-        int num=5; //Binary- 0b101  Hexadecimal- 0x7E
+        int num=541; //Binary- 0b101  Hexadecimal- 0x7E
         long l =54367l;
         float f = 52.8f;
         double d = 12e11; 
         boolean o =true;
 
-        System.out.println(":== " +d+" Hello World!!!");
+        System.out.println("Hello World!!!");
+        //------------------------------
+        //Type Conversion and Casting
+        //------------------------------
+        byte a = (byte)num; //modulus converstion, as num value exceed the range of byte it will be num % 256 (2^8)
+        int result = a+b; // addition of 2 byte values which exceed the byte range would be converted to respective data type by java automatically. 
+        System.out.println(result);
+        
     }
 }
